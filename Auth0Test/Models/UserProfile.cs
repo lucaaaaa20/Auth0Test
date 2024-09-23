f0ca6f20ec95eab0e15459187cefe96af21f9e2b
lucaaaaa20 <lucaadriani59@gmail.com>
2024-09-23 20:20:44 +0200

    Aggiungere i file di progetto.

---
+++ Auth0Test/Models/UserProfile.cs
@@ -0,0 +1,10 @@
+﻿namespace Auth0Test.Models
+{
+    public class UserProfile
+    {
+        public string EmailAddress {  get; set; }
+        public string Name { get; set; }
+        public string ProfileImage { get; set; }
+        public string UserId { get; set; }
+    }
+}
